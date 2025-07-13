ba885a17592930e2b0f3f4f70cafc0e527bc305f
Kirill Erenkov <k.erenkov@adriver.ru>
2025-07-13 11:29:00 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/crypto-brains">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crypto-brains/',
+  base: '/cr/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
